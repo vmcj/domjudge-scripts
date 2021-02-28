1ddce265a5359237beef46751e1d6aa4d02d33b5
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-02-28 13:27:54 +0100

    shellcheck fixes

--- DOMjudge_tag_release.sh
+++ DOMjudge_tag_release.sh
@@ -5,70 +5,73 @@
 DOWNLOAD_RELEASE_SCRIPT=""
 
 notify_channel () {
-    logger $1
+    logger "$1"
     # Optionally notify the Slack channel
     # that a build has been triggered
 }
 
 test_tarball () {
     # Happens as current side effect of docker build
+    true
 }
 
 build_deb () {
     # Not sure if this should happen on the server?
+    true
 }
 
 build_docker () {
     # Create on: https://gitlab.com/DOMjudge/domjudge-packaging/-/settings/ci_cd
     curl -X POST \
-         -F token=$TOKEN \
+         -F token="$TOKEN" \
          -F "ref=master" \
          -F "variables[DOMJUDGE_VERSION]=$1" \
          https://gitlab.com/api/v4/projects/24097565/trigger/pipeline
 }
 
 process_tag () {
-    if [ ! -f $RELEASE_DIR/$1 ]; then
+    if [ ! -f "$RELEASE_DIR/$1" ]; then
         notify_channel "Found new tag: ($1)."
         if [ -z $DOWNLOAD_RELEASE_SCRIPT ]; then
-            wget $RELEASE_WEB
-            $DOWNLOAD_RELEASE_SCRIPT="done"
+            wget "$RELEASE_WEB"
+            DOWNLOAD_RELEASE_SCRIPT="done"
         fi
-        TRUST=$(git tag -v $1)
-        if [ ! $TRUST ]; then
+        if git tag -v "$1"; then
             notify_channel "Untrusted tag ($1)"
             exit 1
         fi
-        ./make_release $1
-        rsync -rva ./$1* $RELEASE_DIR/
+        ./make_release "$1"
+        rsync -rva "./$1"* "$RELEASE_DIR/"
         notify_channel "Tarball finished ($1)."
-        test_tarball $1
-        build_docker $1
-        build_deb $1
-        if [ $2 =eq 1 ]; then
+        test_tarball "$1"
+        build_docker "$1"
+        build_deb "$1"
+        if [ "$2" -eq 1 ]; then
             # Release on the main branch,
             # This is the latest release
             #
             # TODO: Handle website update
+            true
         fi
     fi
 }
 
 process_branch () {
-    while read tag
+    while read -r tag
     do
-        echo "Handling tag: " $tag " in branch: " $1
-        process_tag $tag
-    done <<< $(git tag)
+        echo "Handling tag: $tag in branch: $1"
+        process_tag "$tag"
+    done <<< "$(git tag)"
 }
 
 # Reset to master
 git checkout master
 
+# The main branch is special as we also update the website
 process_branch origin/master 1
 
-while read branch
+while read -r branch
 do
-    process_branch $branch 0
-done <<< $(git branch -r | grep "origin")
+    process_branch "$branch" 0
+done <<< "$(git branch -r)"
 
