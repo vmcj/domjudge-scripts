fbb1df3628d322360bc3d93e2327bf0eedb6fdaa
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-03-20 16:44:14 +0100

    Dont switch branches

--- DOMjudge_tag_release.sh
+++ DOMjudge_tag_release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -eu
+#set -eu
 
-. ./release.env
+#. ./release.env
+main=master
 
 DOWNLOAD_RELEASE_SCRIPT=""
 
@@ -27,11 +28,6 @@ test_tarball () {
     true
 }
 
-build_deb () {
-    # Not sure if this should happen on the server?
-    true
-}
-
 build_docker () {
     # Create on: https://gitlab.com/DOMjudge/domjudge-packaging/-/settings/ci_cd
     curl -X POST \
@@ -42,47 +38,72 @@ build_docker () {
 }
 
 process_tag () {
-    if [ ! -f "$RELEASE_DIR/$1" ]; then
-        notify_channel "Found new tag: ($1)."
-        if [ -z $DOWNLOAD_RELEASE_SCRIPT ]; then
-            wget "$RELEASE_WEB"
-            DOWNLOAD_RELEASE_SCRIPT="done"
-        fi
-        if git tag -v "$1"; then
-            notify_channel "Untrusted tag ($1)"
-            exit 1
-        fi
-        ./make_release "$1"
-        rsync -rva "./$1"* "$RELEASE_DIR/"
-        notify_channel "Tarball finished ($1)."
-        test_tarball "$1"
-        build_docker "$1"
-        build_deb "$1"
-        if [ "$2" -eq 1 ]; then
-            # Release on the main branch,
-            # This is the latest release
-            #
-            # TODO: Handle website update
-            docs_version "$tag"
-            true
-        fi
+    TAG="$1"
+    NUMB="[0-9]+"
+    DOT="\."
+    RELEASE="$NUMB$DOT$NUMB$DOT$NUMB"
+    OPTRC="((RC|rc)[0-9])?"
+    if [[ $TAG =~ ^$RELEASE$OPTRC$ ]]; then
+       # TODO: check if the file already exists
+       echo "Verify $TAG"
+       # To find the key of Thijs:
+       # gpg --search 780355B5EA6BFC8235A99C4B56F61A79401DAC04
+       # And if one trusts the internet to be correct
+       # gpg --recv-keys 780355B5EA6BFC8235A99C4B56F61A79401DAC04
+       if git verify-tag $TAG; then
+           echo "Valid $TAG"
+       else
+           echo "Unknown $TAG"
+       fi
+    #else
+    #   echo "Skip: $TAG"
     fi
+    #if [ ! -f "$RELEASE_DIR/$TAG" ]; then
+    #    notify_channel "Found new tag: ($1)."
+    #    if [ -z $DOWNLOAD_RELEASE_SCRIPT ]; then
+    #        wget "$RELEASE_WEB"
+    #        DOWNLOAD_RELEASE_SCRIPT="done"
+    #    fi
+    #    if git tag -v "$1"; then
+    #        notify_channel "Untrusted tag ($1)"
+    #        exit 1
+    #    fi
+    #    ./make_release "$1"
+    #    rsync -rva "./$1"* "$RELEASE_DIR/"
+    #    notify_channel "Tarball finished ($1)."
+    #    test_tarball "$1"
+    #    build_docker "$1"
+    #    build_deb "$1"
+    #    if [ "$2" -eq 1 ]; then
+    #        # Release on the main branch,
+    #        # This is the latest release
+    #        #
+    #        # TODO: Handle website update
+    #        docs_version "$tag"
+    #        true
+    #    fi
+    #fi
 }
 
-process_branch () {
-    while read -r tag; do
-        echo "Handling tag: $tag in branch: $1"
-        process_tag "$tag"
-    done <<< "$(git tag)"
-}
+#process_branch () {
+#    while read -r tag; do
+#        echo "Handling tag: $tag in branch: $1"
+#        process_tag "$tag"
+#    done <<< "$(git tag)"
+#}
 
 # Reset to master
-git checkout master
+git checkout $main
 
-# The main branch is special as we also update the website
-process_branch origin/master 1
+while read -r tag; do
+    #echo "Handling tag: $tag"
+    process_tag "$tag"
+done <<< "$(git tag)"
 
-while read -r branch; do
-    process_branch "$branch" 0
-done <<< "$(git branch -r)"
+# The main branch is special as in the future we also update the website
+# process_branch origin/$main 1
+
+#while read -r branch; do
+#    process_branch "$branch" 0
+#done <<< "$(git branch -r)"
 
