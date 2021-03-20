cb0bb3b5d392e2cf124a2c1ee8f4e7b1e3cd3f46
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-03-20 22:11:08 +0100

    Should work on the server

--- DOMjudge_tag_release.sh
+++ DOMjudge_tag_release.sh
@@ -4,9 +4,11 @@ set -eu
 
 main=master
 
+RELEASE_DIR="/srv/http/domjudge/releases"
 DOWNLOAD_RELEASE_SCRIPT=""
 
 notify_channel () {
+    echo "$1"
     logger "$1"
     # Optionally notify the Slack channel
     # that a build has been triggered
@@ -23,6 +25,10 @@ process_tag () {
        if [ -f "$RELEASE_DIR/domjudge-$TAG.tar.gz" ]; then
            # Tag is already handled
            return 0
+       else
+           echo "We should build domjudge-$TAG.tar.gz"
+           echo "In: $RELEASE_DIR/domjudge-$TAG.tar.gz"
+       fi
        # To find the signer key of a earlier tag:
        # gpg --search 780355B5EA6BFC8235A99C4B56F61A79401DAC04
        # And if one trusts the internet to be correct
@@ -30,18 +36,18 @@ process_tag () {
        set +e # Some tags are not signed
        if git verify-tag $TAG; then
            set -e
-           echo "Valid $TAG"
            # At this point the tarball should already be locally tested
-           ./make_release "$TAG"
+           ~domjudge-scripts/make_release.sh "$TAG"
+           mv domjudge-$TAG.* $RELEASE_DIR/
            notify_channel "Tarball finished ($TAG)."
        else
-           # In this case we should probably alert on such a tag?
            notify_channel "Untrusted tag ($TAG)"
        fi
     fi
 }
 
 # Reset to main branch
+cd ~domjudge/domjudge
 git checkout $main
 
 while read -r tag; do
